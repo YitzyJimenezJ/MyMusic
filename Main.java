fdbbac8562cf27e68cf0ac7a25934cee3148fb90
YitzyJimenezJ <jimenezyitzy17@gmail.com>
2022-05-24 13:56:34 -0600

    PartedeJava

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        //ManejadorCanciones manejadorCanciones = new ManejadorCanciones(); 
+        //manejadorCanciones.guardarCancion("Prueba", "genero", "artista", "album", "letra", 20);
+        System.out.println("hola  mundo");
+
+        //Servidor server = new Servidor(5566);
+        
+    }
+}
